2dd298a55c6f0ca82d7273d2ba9135726ab0a941
Karl Matthes <karlmatthes@gmail.com>
2019-11-05 02:50:51 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
@@ -18,5 +19,24 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  
+  nds.length.times do |cycle_directors|
+    total_gross = 0
+    
+    nds[cycle_directors][:movies].length.times do |cycle_titles|
+      total_gross += nds[cycle_directors][:movies][cycle_titles][:worldwide_gross]
+    end
+    
+    result[nds[cycle_directors][:name]] = total_gross
+    
+  end
+  
+  return result
+  
 end
+
+#pp directors_database[0] # directors
+#pp directors_database[0][:movies] #movies
+#pp directors_database[0][:movies][0] #single title
+#pp directors_database[0][:movies][0][:worldwide_gross] #gross
+##pp directors_totals(directors_database)
